2a494959686a7c8491b200e4815a0fc0fdb35d51
ecasdqina <e1711@s.akashi.ac.jp>
2020-03-18 16:13:01 +0900

    fix modint

--- math/modint.hpp
+++ math/modint.hpp
@@ -12,33 +12,38 @@ public:
 	u64 a;
 	static constexpr u64 mod = Modulus;
 
-	constexpr modint(const u64 & x = 0) noexcept : a(x % Modulus) {}
+	constexpr modint(const i64& x = 0) noexcept: a(apply(x % Modulus)) {}
 
-	constexpr u64 &value() noexcept { return a; }
-	constexpr const u64 &value() const noexcept { return a; }
-
-	const modint inverse() const {
-		return modint(1) / *this;
-	}
-	const modint pow(i64 k) const {
-		return modint(*this) ^ k;
-	}
-
-	constexpr modint & operator+=(const modint & rhs) noexcept {
+	constexpr modint operator+(const modint& rhs) const noexcept { return modint(*this) += rhs; }
+	constexpr modint operator-(const modint& rhs) const noexcept { return modint(*this) -= rhs; }	
+	constexpr modint operator*(const modint& rhs) const noexcept { return modint(*this) *= rhs; }
+	constexpr modint operator/(const modint& rhs) const noexcept { return modint(*this) /= rhs; }
+	constexpr modint operator^(const u64& k) const noexcept { return modint(*this) ^= k; }
+	constexpr modint operator^(const modint& k) const noexcept { return modint(*this) ^= k.value(); }
+	constexpr modint operator-() const noexcept { return modint(Modulus - a); }
+	constexpr modint operator++() noexcept { return (*this) = modint(*this) + 1; }
+	constexpr modint operator--() noexcept { return (*this) = modint(*this) - 1; }
+	const bool operator==(const modint& rhs) const noexcept { return a == rhs.a; };
+	const bool operator!=(const modint& rhs) const noexcept { return a != rhs.a; };
+	const bool operator<=(const modint& rhs) const noexcept { return a <= rhs.a; };
+	const bool operator>=(const modint& rhs) const noexcept { return a >= rhs.a; };
+	const bool operator<(const modint& rhs) const noexcept { return a < rhs.a; };
+	const bool operator>(const modint& rhs) const noexcept { return a > rhs.a; };
+	constexpr modint& operator+=(const modint& rhs) noexcept {
 		a += rhs.a;
 		if (a >= Modulus) a -= Modulus;
 		return *this;
 	}
-	constexpr modint & operator-=(const modint & rhs) noexcept {
+	constexpr modint& operator-=(const modint& rhs) noexcept {
 		if (a < rhs.a) a += Modulus;
 		a -= rhs.a;
 		return *this;
 	}
-	constexpr modint & operator*=(const modint & rhs) noexcept {
+	constexpr modint& operator*=(const modint& rhs) noexcept {
 		a = a * rhs.a % Modulus;
 		return *this;
 	}
-	constexpr modint & operator/=(modint rhs) noexcept {
+	constexpr modint& operator/=(modint rhs) noexcept {
 		u64 exp = Modulus - 2;
 		while (exp) {
 			if (exp % 2) (*this) *= rhs;
@@ -48,7 +53,7 @@ public:
 		}
 		return *this;
 	}
-	constexpr modint & operator^=(u64 k) noexcept {
+	constexpr modint& operator^=(u64 k) noexcept {
 		auto b = modint(1);
 		while(k) {
 			if(k & 1) b = b * (*this);
@@ -57,31 +62,27 @@ public:
 		}
 		return (*this) = b;
 	}
-	constexpr modint & operator=(const modint & rhs) noexcept {
+	constexpr modint& operator=(const modint& rhs) noexcept {
 		a = rhs.a;
 		return (*this);
 	}
-	constexpr modint operator+(const modint & rhs) const noexcept { return modint(*this) += rhs; }
-	constexpr modint operator-(const modint & rhs) const noexcept { return modint(*this) -= rhs; }	
-	constexpr modint operator*(const modint & rhs) const noexcept { return modint(*this) *= rhs; }
-	constexpr modint operator/(const modint & rhs) const noexcept { return modint(*this) /= rhs; }
-	constexpr modint operator^(const u64 & k) const noexcept { return modint(*this) ^= k; }
-	constexpr modint operator-() const noexcept { return modint(Modulus - a); }
-	constexpr modint operator++() noexcept { return (*this) = modint(*this) + 1; }
-	constexpr modint operator--() noexcept { return (*this) = modint(*this) - 1; }
-	const bool operator==(const modint & rhs) const noexcept { return a == rhs.a; };
-	const bool operator!=(const modint & rhs) const noexcept { return a != rhs.a; };
-	const bool operator<=(const modint & rhs) const noexcept { return a <= rhs.a; };
-	const bool operator>=(const modint & rhs) const noexcept { return a >= rhs.a; };
-	const bool operator<(const modint & rhs) const noexcept { return a < rhs.a; };
-	const bool operator>(const modint & rhs) const noexcept { return a > rhs.a; };
+
+	constexpr u64& value() noexcept { return a; }
+	constexpr const u64& value() const noexcept { return a; }
 	explicit operator bool() const { return a; }
 	explicit operator u32() const { return a; }
 
-	friend std::ostream & operator<<(std::ostream & os, const modint & p) {
+	const modint inverse() const {
+		return modint(1) / *this;
+	}
+	const modint pow(i64 k) const {
+		return modint(*this) ^ k;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const modint& p) {
 		return os << p.a;
 	}
-	friend std::istream & operator>>(std::istream & is, modint & p) {
+	friend std::istream& operator>>(std::istream& is, modint& p) {
 		u64 t;
 		is >> t;
 		p = modint(t);
