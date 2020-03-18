0ccf92d2dd5607ad4945281e8aded151eacc4ad3
ecasdqina <e1711@s.akashi.ac.jp>
2020-03-18 12:59:19 +0900

    add polynomial

---
+++ math/polynomial.hpp
@@ -0,0 +1,94 @@
+#include <cstdint>
+#include <vector>
+
+template<class T>
+class polynomial: public std::vector<T> {
+public:
+	using std::vector<T>::vector;
+	using value_type = typename std::vector<T>::value_type;
+	using reference = typename std::vector<T>::reference;
+	using const_reference = typename std::vector<T>::const_reference;
+	using size_type = typename std::vector<T>::size_type;
+
+private:
+	T eval(T x) const {
+		T ret = (*this)[0], tmp = x;
+		for(int i = 1; i < this->size(); i++) {
+			ret = ret + (*this)[i] * tmp;
+			tmp = tmp * x;
+		}
+		return ret;
+	}
+
+public:
+	polynomial(): std::vector<T>(1, T{}) {}
+
+	polynomial operator+(const polynomial& r) const { return polynomial(*this) *= r; }
+	polynomial operator+(const_reference r) const { return polynomial(*this) *= r; }
+	polynomial operator-(const polynomial& r) const { return polynomial(*this) *= r; }
+	polynomial operator-(const_reference r) const { return polynomial(*this) *= r; }
+	polynomial operator<<(size_type r) const { return polynomial(*this) <<= r; }
+	polynomial operator>>(size_type r) const { return polynomial(*this) >>= r; }
+	polynomial operator-() const {
+		polynomial ret(this->size());
+		for(int i = 0; i < this->size(); i++) ret[i] = -(*this)[i];
+		return ret;
+	}
+	polynomial& operator+=(const polynomial& r) {
+		if(r.size() > this->size()) this->resize(r.size());
+		for(int i = 0; i < r.size(); i++) (*this)[i] = (*this)[i] + r[i];
+		return *this;
+	}
+	polynomial& operator-=(const polynomial& r) {
+		if(r.size() > this->size()) this->resize(r.size());
+		for(int i = 0; i < r.size(); i++) (*this)[i] = (*this)[i] + r[i];
+		return *this;
+	}
+	polynomial& operator*=(const_reference r) {
+		for(int i = 0; i < this->size(); i++) (*this)[i] = (*this)[i] * r;
+		return *this;
+	}
+	polynomial& operator/=(const_reference r) {
+		for(int i = 0; i < this->size(); i++) (*this)[i] = (*this)[i] / r;
+		return *this;
+	}
+	polynomial& operator<<=(size_type r) {
+		this->insert(begin(*this), r, T{});
+		return *this;
+	}
+	polynomial& operator>>=(size_type r) {
+		if(r >= this->size()) clear();
+		else this->erase(begin(*this), begin(*this) + r);
+		return *this;
+	}
+
+	polynomial differential(size_type k) const {
+		polynomial ret(*this);
+		for(int i = 0; i < k; i++) ret = ret.differential();
+		return ret;
+	}
+	polynomial differential() const {
+		if(degree() < 1) return polynomial();
+		polynomial ret(this->size() - 1);
+		for(int i = 1; i < this->size(); i++) ret[i - 1] = (*this)[i] * T{i};
+		return ret;
+	}
+	polynomial integral(size_type k) const {
+		polynomial ret(*this);
+		for(int i = 0; i < k; i++) ret = ret.integral();
+		return ret;
+	}
+	polynomial integral() const {
+		polynomial ret(this->size() + 1);
+		for(int i = 0; i < this->size(); i++) ret[i + 1] = (*this)[i] / T{i + 1};
+		return ret;
+	}
+	
+	void shrink() {
+		while(this->size() > 1 and this->back() == T{}) this->pop_back();
+	}
+	
+	T operator()(T x) const { return eval(x); }
+	size_type degree() const { return this->size() - 1; }
+	void clear() { this->assign(1, T{}); }
+};
