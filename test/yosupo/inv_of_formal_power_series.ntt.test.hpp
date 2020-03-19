da0d6b81de1d0dba30f03dbe8a80425ca7f5ef8b
ecasdqina <e1711@s.akashi.ac.jp>
2020-03-19 22:39:56 +0900

    add test(inv, exp, log)

---
+++ test/yosupo/inv_of_formal_power_series.ntt.test.hpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series"
+
+#include <stdio.h>
+#include "../../math/number_theoritic_transform.hpp"
+#include "../../math/formal_power_series.hpp"
+
+using fps = formal_power_series<number_theoritic_transform<modint<998244353>>>;
+
+int main() {
+	int n; scanf("%d", &n);
+	fps a(n);
+	for(int i = 0; i < n; i++) {
+		int x; scanf("%d", &x);
+
+		a[i] = x;
+	}
+
+	for(auto v: a.inverse()) printf("%d ", v.value());
+	printf("\n");
+	return 0;
+}
